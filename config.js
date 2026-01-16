5da688b1555f7610e1071c04a50df43bf9900b45
mutonga <romeomutonga3@gmail.com>
2026-01-16 13:11:52 +0300

    Updated

---
+++ config.js
@@ -0,0 +1,25 @@
+// Creating and exporting configuration variables
+
+// Container for all the environments
+const environments = {};
+
+// Staging (default) environment
+environments.staging = {
+    port: 3000,
+    envName: 'staging',
+};
+
+// Production environment
+environments.production = {
+    port: 5000,
+    envName: 'production',
+};
+
+// Determine which environment was passed as a command-line argument
+const currentEnvironment = typeof(process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+// Check that the current environment is one of the environments above, if not, default to staging
+const environmentToExport = typeof(environments[currentEnvironment]) === 'object' ? environments[currentEnvironment] : environments.staging;
+
+// Export the module
+module.exports = environmentToExport;
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // importing necessary modules
 
+const { stat } = require('fs');
 const http = require('http');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
+const config = require('./config');
 
 // creating a server
 const server = http.createServer((req, res) => {
@@ -32,18 +34,52 @@ const server = http.createServer((req, res) => {
 
     req.on('end', () => {
         buffer += decoder.end();
-        // log the payload
-        console.log('Payload:', buffer);
+
+        // call the appropriate handler
+        const handler = router[normalizedPath] || handlers.notFound;
+        handler({ normalizedPath, method, pathname, queryParams, payload: buffer }, (statusCode, payload) => {
+            payload = typeof payload === 'object' ? payload : {};
+            statusCode = typeof statusCode === 'number' ? statusCode : 200;
+
+            const payloadString = JSON.stringify(payload);
+
+            // return the response
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            // log the payload
+            console.log(`Returning this response: ${statusCode} - ${payloadString}`);
+        });
     });
 
-    // send response
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(`Normalized Path: ${normalizedPath}\nMethod: ${method}\n`);
+    // // send response
+    // res.writeHead(200, { 'Content-Type': 'text/plain' });
+    // res.end(`Normalized Path: ${normalizedPath}\nMethod: ${method}\n`);
 
-    console.log('Query Parameters:', queryParams);
+    // console.log('Query Parameters:', queryParams);
 });
 
 // server listening on port 3000
-server.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+server.listen(config.port, () => {
+    console.log(`Server is listening on port ${config.port} in ${config.envName} mode`);
 });
+
+
+// Define handlers
+const handlers = {};
+
+// Sample handler
+handlers.sample = (data, callback) => {
+    callback(200, { message: 'This is a sample handler' });
+};
+
+// Not found handler
+handlers.notFound = (data, callback) => {
+    callback(404);
+};
+
+// Define a request router
+const router = {
+    'sample' : handlers.sample
+};
