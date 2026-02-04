44d15f19307cb1045f1a93f870de6147bd6a5dee
mutonga <romeomutonga3@gmail.com>
2026-02-04 15:56:04 +0300

    Updated

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ environments.staging = {
     httpPort: 3000,
     httpsPort: 3001,
     envName: 'staging',
+    maxChecks: 5
 };
 
 // Production environment
@@ -15,6 +16,7 @@ environments.production = {
     httpPort: 5000,
     httpsPort: 5001,
     envName: 'production',
+    maxChecks: 5
 };
 
 // Determine which environment was passed as a command-line argument
